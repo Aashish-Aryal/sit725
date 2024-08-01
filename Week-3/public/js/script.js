d66ea6172729b2832277d27c5b5b1ef1e335f833
Aashish-Aryal <mail.aashisharyal@gmail.com>
2024-08-01 15:12:14 +1000

    added logging

--- Week-3/public/js/script.js
+++ Week-3/public/js/script.js
@@ -1,23 +1,31 @@
 $(document).ready(function () {
     $("form").submit(function (event) {
-      const formData = {
-        first_name: $("#first_name").val(),
-        last_name: $("#last_name").val(),
-        email: $("#email").val(),
-        password: $("#password").val()
+        event.preventDefault();
+        
+        const formData = {
+            first_name: $("#first_name").val(),
+            last_name: $("#last_name").val(),
+            email: $("#email").val(),
+            password: $("#password").val()
       };
+
+      console.log("Submitting form data:", formData);
   
       $.ajax({
         type: "POST",
         url: "http://127.0.0.1:3040/signup",
-        data: formData,
+        contentType: "application/json",        
+        data: JSON.stringify(formData),
         dataType: "json",
-        // encode: true,
-        success: function(d) {
-            alert(d);
+        success: function(response) {
+            console.log("Response from server:", response);
+            alert(response.message);
+        },
+        error: function (xhr, status, error) {
+            console.log("Error response from server:", xhr.responseText);
+            alert(`Error: ${xhr.responseText}`)
         }
       })
 
-      event.preventDefault();
     });
   });
--- Week-3/server_get.js
+++ Week-3/server_get.js
@@ -1,30 +1,28 @@
-    const cors= require("cors");
-    
-    const express= require("express");
-    const app= express();
-    
-    const users = []
+const cors= require("cors");
+const express= require("express");
+const app= express();
+const users = []
 
-    const isEmailUnique = (users, email) =>{
-        const emailExists = users.find(user => user.email.toLowerCase() === email.toLowerCase())
-        return !emailExists
+const isEmailUnique = (users, email) =>{
+    const emailExists = users.find(user => user.email.toLowerCase() === email.toLowerCase());
+    return !emailExists;
+};
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors({ origin: 'http://127.0.0.1:3040' }));
+
+app.post("/signup", (req,res)=>{
+    console.log("Received sign-up request:", req.body);
+    const { first_name, last_name, email, password } = req.body;
+    if (!isEmailUnique(users, email)) {
+        return res.status(400).send({message:"Email already exists."});
     }
+    users.push({first_name, last_name, email, password});
+    return res.status(201).send({message:"Sign Up Successful"});
+});
 
-    app.use(express.static('public'))
-
-    app.use(express.json())
-    app.use(cors({ origin: 'http://127.0.0.1:3040' }))
-    app.post("/signup", (req,res)=>{
-        console.log("AAyo")
-        const {first_name, last_name, email, password}=req.body
-        if (!isEmailUnique(users, email)) {
-            return res.status(400).send({message:"Email already exists."})
-        }
-        users.push({first_name, last_name, email, password})
-        return res.status(201).send({message:"Sign Up Successful"})
-    });
-
-    const port=3040;
-    app.listen(port,()=> {
-        console.log("hello i'm listening to port "+port);
-    }) 
+const port=3040;
+app.listen(port,()=> {
+    console.log("hello i'm listening to port "+port);
+}) 
